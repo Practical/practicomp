3c2776153724b7578e541a60c6c423f5f06c014f
Shachar Shemesh <shachar@shemesh.biz>
2019-06-09 11:23:18 +0300

    Add missing copyright headers

--- compiler/utils.h
+++ compiler/utils.h
@@ -1,3 +1,11 @@
+/* This file is part of the Practical programming langauge. https://github.com/Practical/practical-sa
+ *
+ * To the extent header files enjoy copyright protection, this file is file is copyright (C) 2018-2019 by its authors
+ * You can see the file's authors in the AUTHORS file in the project's home repository.
+ *
+ * This is available under the Boost license. The license's text is available under the LICENSE file in the project's
+ * home directory.
+ */
 #ifndef UTILS_H
 #define UTILS_H
 
